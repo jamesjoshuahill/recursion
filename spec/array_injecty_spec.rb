d2e9e3895651b050a429210af9474d14b8388b33
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-13 16:41:27 +0100

    Split out expectations in injecty spec

--- spec/array_injecty_spec.rb
+++ spec/array_injecty_spec.rb
@@ -15,26 +15,53 @@ describe Array do
     end
 
     context 'when a block is given' do
-      it 'should return the block evaluation for an empty array' do
-        array = Array.new
-        expect(array.injecty { 1 } ).to eq 1
-        expect(array.injecty { 500 / 5 }).to eq 100
+      context 'should return the block evaluation for an empty array' do
+        let(:array) { Array.new }
+
+        it 'when passing one' do
+          expect(array.injecty { 1 } ).to eq 1
+        end
+
+        it 'when passing in a computation' do
+          expect(array.injecty { 500 / 5 }).to eq 100
+        end
       end
 
-      it 'should apply the block to the element of singleton array' do
-        array = [5]
-        expect(array.injecty { 'a' }).to eq 'a'
-        expect(array.injecty { |memo, num| memo * num }).to eq 25
-        expect(array.injecty { |memo, num| memo + (num * 5) }).to eq 30
+      context 'should apply the block to the element of singleton array' do
+        let(:array) { [5] }
+
+        it 'when passing a block with a string' do
+          expect(array.injecty { 'a' }).to eq 'a'
+        end
+
+        it 'when passing a block with a multiplication' do
+          expect(array.injecty { |memo, num| memo * num }).to eq 25
+        end
+
+        it 'when passing a block with a computation' do
+          expect(array.injecty { |memo, num| memo + (num * 5) }).to eq 30
+        end
       end
 
-      it 'should apply the block to all elements of an array' do
-        array = [1, 2, 3]
-        expect(array.injecty { 'a' }).to eq 'a'
-        expect(array.injecty { |memo, num| memo * num }).to eq 6
-        expect(array.injecty { |memo, num| memo + (num * 5) }).to eq 31
-        array = ['a', 'b', 'c']
-        expect(array.inject { |memo, char| memo << char }).to eq 'abc'
+      context 'should apply the block to all elements of an array' do
+        let(:array) { [1, 2, 3] }
+
+        it 'when passing a block with a string' do
+          expect(array.injecty { 'a' }).to eq 'a'
+        end
+
+        it 'when passing a block with a multiplication' do
+          expect(array.injecty { |memo, num| memo * num }).to eq 6
+        end
+
+        it 'when passing a block with a computation' do
+          expect(array.injecty { |memo, num| memo + (num * 5) }).to eq 31
+        end
+
+        it 'when passing a block to concatenate strings' do
+          array = ['a', 'b', 'c']
+          expect(array.inject { |memo, char| memo << char }).to eq 'abc'
+        end
       end
     end
   end
