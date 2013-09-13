823ae371366b6634bd8ebb750df68c4aa9c6c5ad
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-13 17:07:16 +0100

    Minor improvement to injecty code wording

--- lib/array_injecty.rb
+++ lib/array_injecty.rb
@@ -1,20 +1,20 @@
 class Array
   def injecty memo=self.first, &block
-    return nil_or_raise_error if !block_given?
+    return nil_if_empty_or_raise_error if !block_given?
     return yield if self.empty?
 
     self.evaluate_all_elements_with(memo, &block)
   end
 
-  def nil_or_raise_error
+  def nil_if_empty_or_raise_error
     return nil if self.empty?
     raise LocalJumpError, 'no block given'
   end
 
   def evaluate_all_elements_with memo, &block
-    first_evaluated = yield memo, self.first
-    return first_evaluated if self.length == 1
+    first_element_evaluated = yield memo, self.first
+    return first_element_evaluated if self.length == 1
 
-    self.drop(1).evaluate_all_elements_with(first_evaluated, &block)
+    self.drop(1).evaluate_all_elements_with(first_element_evaluated, &block)
   end
 end
